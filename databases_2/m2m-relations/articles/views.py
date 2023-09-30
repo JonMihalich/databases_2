bad13dac0c01f173e4a49bfb14a21581f649ea73
Jon Mihal <karimilohina@yandex.ru>
2023-09-30 13:45:45 +0200

    initial

--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -1,4 +1,3 @@
-from django.views.generic import ListView
 from django.shortcuts import render
 
 from articles.models import Article
@@ -6,10 +5,11 @@ from articles.models import Article
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = '-published_at'
-
+    articles = Article.objects.order_by(ordering).prefetch_related('tags')
+    context = {
+        'object_list': articles,
+    }
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     return render(request, template, context)
